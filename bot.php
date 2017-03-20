b4510e34a828e238b8ad9f01e9282837ed461ee8
MoreThrust <peeraphat1997@gmail.com>
2017-03-20 12:57:16 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -9,20 +9,18 @@ $events = json_decode($content, true);
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
-		
-		{
-  "type": "template",
-  "altText": "this is a buttons template",
-  "template": {
-      "type": "buttons",
-      "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
-      "title": "Menu",
-      "text": "Please select",
-      "actions": [
-          {
-            "type": "postback",
-            "label": "Buy",
-            "data": "action=buy&itemid=123"
+		"type": "template",
+  		"altText": "this is a buttons template",
+ 		 "template": {
+     		 "type": "buttons",
+     		 "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+     		  "title": "Menu",
+   		  "text": "Please select",
+  		   "actions": [
+  		       {
+            	"type": "postback",
+   		         "label": "Buy",
+     	       "data": "action=buy&itemid=123"
           },
           {
             "type": "postback",
@@ -36,41 +34,6 @@ if (!is_null($events['events'])) {
           }
       ]
   }
-}
-		
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => 'hey'
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-		}
 	}
 }
 echo "OK";
