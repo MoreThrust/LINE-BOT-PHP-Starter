92733e1d2e6e22cbcc0b8106c114530ca3197a3f
MoreThrust <peeraphat1997@gmail.com>
2017-03-20 13:02:56 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -9,31 +9,39 @@ $events = json_decode($content, true);
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
-		"type": "template",
-  		"altText": "this is a buttons template",
- 		 "template": {
-     		 "type": "buttons",
-     		 "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
-     		  "title": "Menu",
-   		  "text": "Please select",
-  		   "actions": [
-  		       {
-            	"type": "postback",
-   		         "label": "Buy",
-     	       "data": "action=buy&itemid=123"
-          },
-          {
-            "type": "postback",
-            "label": "Add to cart",
-            "data": "action=add&itemid=123"
-          },
-          {
-            "type": "uri",
-            "label": "View detail",
-            "uri": "http://example.com/page/123"
-          }
-      ]
-  }
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'hi' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => 'hey'
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}
 	}
 }
 echo "OK";
