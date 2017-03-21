a8825505dd887ed2bfcc611150da2b9d33f8ec49
MoreThrust <peeraphat1997@gmail.com>
2017-03-21 08:15:37 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -34,25 +34,25 @@ if (!is_null($events['events'])) {
 			echo $result . "\r\n";
 		}
 
-        if ($event['message']['text'] == "เปิดไฟ") {
+        if ($event['message']['text'] == "on") {
 			$replyToken = $event['replyToken'];
 
 			$messages = [
-				"type": "template",
-                "altText": "this is a confirm template",
-                "template": {
-                    "type": "confirm",
-                    "text": "Are you sure?",
-                    "actions": [
+				'type' => 'template',
+                'altText' => 'this is a confirm template',
+                'template' => {
+                    'type' => 'confirm',
+                    'text' => 'Are you sure?',
+                    'actions' => [
                     {
-                    "type": "message",
-                    "label": "Yes",
-                    "text": "yes"
+                    'type' => 'message',
+                    'label' => 'Yes',
+                    'text' => 'yes'
                     },
                     {
-                    "type": "message",
-                    "label": "No",
-                    "text": "no"
+                    'type' => 'message',
+                    'label' => 'No',
+                    'text' => 'no'
                     }
                     ]
                 }
