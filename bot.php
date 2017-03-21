b87e1bc1ad994adb01aa814411bd32e07b7113ef
MoreThrust <peeraphat1997@gmail.com>
2017-03-21 08:32:58 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -70,18 +70,8 @@ if (!is_null($events['events'])) {
                 'altText' => 'this is a confirm template',
                 'template' => {
                     'type' => 'confirm',
-                    'text' => 'Are you sure?',
-                    'actions' => [{
-                        'type' => 'message',
-                        'label' => 'Yes',
-                        'text' => 'yes'
-                        },
-                        {
-                        'type' => 'message',
-                        'label' => 'No',
-                        'text' => 'no'
-                        }
-                        ]}
+                    'text' => 'Are you sure?'
+                    }
 			];
 
 			$url = 'https://api.line.me/v2/bot/message/reply';
