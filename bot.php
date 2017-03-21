37547a60b79f44464c4e0c0fa6ab5d4092b5ab8b
MoreThrust <peeraphat1997@gmail.com>
2017-03-21 08:23:21 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -62,49 +62,7 @@ if (!is_null($events['events'])) {
 			echo $result . "\r\n";
 		}
 
-        if ($event['message']['text'] == "on") {
-			$replyToken = $event['replyToken'];
-
-			$messages = [
-				'type' => 'template',
-                'altText' => 'this is a confirm template',
-                'template' => {
-                    'type' => 'confirm',
-                    'text' => 'Are you sure?',
-                    'actions' => [
-                    {
-                    'type' => 'message',
-                    'label' => 'Yes',
-                    'text' => 'yes'
-                    },
-                    {
-                    'type' => 'message',
-                    'label' => 'No',
-                    'text' => 'no'
-                    }
-                    ]
-                }
-			];
-
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-		}
+        
 
 	}
 }
