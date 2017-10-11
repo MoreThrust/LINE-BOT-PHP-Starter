24f576501d43d56ced305b9c784cd0cf585c050c
MoreThrust <peeraphat1997@gmail.com>
2017-10-11 13:24:17 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,16 +10,14 @@ if (!is_null($events['events'])) {
   // Loop through each event
   foreach ($events['events'] as $event) {
     // Reply only when message sent is in 'text' format
-    if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-      // Get text sent
-      $text = $event['message']['text'];
+    if ($event['message']['text'] == "ok") {
       // Get replyToken
       $replyToken = $event['replyToken'];
 
       // Build message to reply back
       $messages = [
         'type' => 'text',
-        'text' => $text
+        'text' => "5555"
       ];
 
       // Make a POST Request to Messaging API to reply to sender
