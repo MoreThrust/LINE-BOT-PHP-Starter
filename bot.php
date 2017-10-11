49ab0790b5585ac2d2830a30711ffe23ff0efcd8
MoreThrust <peeraphat1997@gmail.com>
2017-10-11 13:30:34 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -16,21 +16,21 @@ if (!is_null($events['events'])) {
 
       // Build message to reply back
       $messages = [{
-          "type": "template",
-          "altText": "this is a confirm template",
-          "template": {
-              "type": "confirm",
-              "text": "Are you sure?",
-              "actions": [
+          'type': 'template',
+          'altText': 'this is a confirm template',
+          'template': {
+              'type': 'confirm',
+              'text': 'Are you sure?',
+              'actions': [
                   {
-                    "type": "message",
-                    "label": "Yes",
-                    "text": "yes"
+                    'type': 'message',
+                    'label': 'Yes',
+                    'text': 'yes'
                   },
                   {
-                    "type": "message",
-                    "label": "No",
-                    "text": "no"
+                    'type': 'message',
+                    'label': 'No',
+                    'text': 'no'
                   }
               ]
           }
