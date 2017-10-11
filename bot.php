6ff2027be58af2e9d90d4a8c217cb042da234506
MoreThrust <peeraphat1997@gmail.com>
2017-10-11 13:38:34 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,8 +18,8 @@ if (!is_null($events['events'])) {
       $messages = [
           
         {
-    "type": "text",
-    "text": "Hello, world"
+    'type'=> 'text',
+    'text'=> 'Hello, world'
 }
         
       ];
