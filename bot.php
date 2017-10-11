393d65a4575e7a22ddd95fcf7b0c5e34ea9e9217
MoreThrust <peeraphat1997@gmail.com>
2017-10-11 13:21:36 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,143 +1,47 @@
 <?php
 $access_token = '1bKxA7ZGjp2P1/6GIBIFiniTJanflttAOEX9svJZKHd4GpTi8fN3VWegRlq1L/wgptw7W7/GvqBFOQTlFYzWNwh3CQy7K0+dKnPuqgZqu9Lj3L/Y2jz8SZnm+Yj7Lqbi9TwbIAGkk2LSA6tD/fWvlwdB04t89/1O/w1cDnyilFU=';
+
+// Get POST body content
 $content = file_get_contents('php://input');
+// Parse JSON
 $events = json_decode($content, true);
-
+// Validate parsed JSON data
 if (!is_null($events['events'])) {
-	foreach ($events['events'] as $event) {
+  // Loop through each event
+  foreach ($events['events'] as $event) {
+    // Reply only when message sent is in 'text' format
+    if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+      // Get text sent
+      $text = $event['message']['text'];
+      // Get replyToken
+      $replyToken = $event['replyToken'];
 
-		if ($event['message']['text'] == "Hi") {
-			$replyToken = $event['replyToken'];
+      // Build message to reply back
+      $messages = [
+        'type' => 'text',
+        'text' => $text
+      ];
 
-			$messages = ['type' => 'text','text' => "ว่าไง มีอะไรให้ช่วยเอ่ย??"];
+      // Make a POST Request to Messaging API to reply to sender
+      $url = 'https://api.line.me/v2/bot/message/reply';
+      $data = [
+        'replyToken' => $replyToken,
+        'messages' => [$messages],
+      ];
+      $post = json_encode($data);
+      $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = ['replyToken' => $replyToken,'messages' => [$messages],];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-		}
+      $ch = curl_init($url);
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+      $result = curl_exec($ch);
+      curl_close($ch);
 
-        if ($event['message']['text'] == "เปิดไฟห้องนอนใหญ่") {
-			$replyToken = $event['replyToken'];
-
-            $messages = ['type' => 'text','text' => "เปิดไฟห้องนอนใหญ่แล้ว"];
-
-            $request = file_get_contents('https://api.anto.io/channel/set/OSZ8RPcqVh2G78Ua2xkqzSnyjrzc0Yp8xFkxHMif/Smart_Home/Lamp1/1');
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = ['replyToken' => $replyToken,'messages' => [$messages],];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-        }
-
-        if ($event['message']['text'] == "ปิดไฟห้องนอนใหญ่") {
-			$replyToken = $event['replyToken'];
-
-			$messages = ['type' => 'text','text' => "ปิดไฟห้องนอนใหญ่แล้ว"];
-           
-            $request = file_get_contents('https://api.anto.io/channel/set/OSZ8RPcqVh2G78Ua2xkqzSnyjrzc0Yp8xFkxHMif/Smart_Home/Lamp1/0');
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = ['replyToken' => $replyToken,'messages' => [$messages],];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-		}
-
-        if ($messages == "ปิดไฟห้องนอนใหญ่แล้ว") {
-			$replyToken = $event['replyToken'];
-
-			$messages = ["type" => "sticker","packageId" => "1","stickerId" => "407"];
-            
-            $request = file_get_contents('https://api.anto.io/channel/set/OSZ8RPcqVh2G78Ua2xkqzSnyjrzc0Yp8xFkxHMif/Smart_Home/Lamp1/0');
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = ['replyToken' => $replyToken,'messages' => [$messages],];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-		}
-
-        if ($event['message']['text'] == "สัส") {
-			$replyToken = $event['replyToken'];
-
-            $messages = ["type" => "location","title" => "มึงไปเล่นตรงนู้นนะ","address" => "โรงพยาบาลศรีธัญญา","latitude" => 13.845162,"longitude" => 100.517254];
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = ['replyToken' => $replyToken,'messages' => [$messages],];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-		}
-
-        if ($event['message']['text'] == "ok") {
-        	$replyToken = $event['replyToken'];
-			$messages = ["type" => "confirm","text" => "Are you sure?","actions" => '[{"type": "message","label": "Yes","text": "yes"},{"type": "message","label": "No","text": "no"}]'];
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = ['replyToken' => $replyToken,'messages' => [$messages],];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-		}
-
-        if ($event['message']['text'] == "chk") {
-			$replyToken = $event['replyToken'];
-
-            $requestL1 = file_get_contents('https://api.anto.io/channel/get/OSZ8RPcqVh2G78Ua2xkqzSnyjrzc0Yp8xFkxHMif/Smart_Home/Lamp1');
-            $requestL2 = file_get_contents('https://api.anto.io/channel/get/OSZ8RPcqVh2G78Ua2xkqzSnyjrzc0Yp8xFkxHMif/Smart_Home/Lamp2');
-            $requestL3 = file_get_contents('https://api.anto.io/channel/get/OSZ8RPcqVh2G78Ua2xkqzSnyjrzc0Yp8xFkxHMif/Smart_Home/Lamp3');
-            $requestA1 = file_get_contents('https://api.anto.io/channel/get/OSZ8RPcqVh2G78Ua2xkqzSnyjrzc0Yp8xFkxHMif/Smart_Home/Air1');
-            $requestA2 = file_get_contents('https://api.anto.io/channel/get/OSZ8RPcqVh2G78Ua2xkqzSnyjrzc0Yp8xFkxHMif/Smart_Home/Air2');
-            $requestA3 = file_get_contents('https://api.anto.io/channel/get/OSZ8RPcqVh2G78Ua2xkqzSnyjrzc0Yp8xFkxHMif/Smart_Home/Air3');
-            
-            if($requestL1 == '{"result":"true","value":"1"}'){
-                $Lamp1 = "ไฟห้องนอนใหญ่: เปิดอยู่";
-            }else{
-                $Lamp1 = "ไฟห้องนอนใหญ่: ปิดอยู่";
-            }
-            if($requestL2 == '{"result":"true","value":"1"}'){
-                $Lamp2 = "ไฟห้องนอนเล็ก: เปิดอยู่";
-            }else{
-                $Lamp2 = "ไฟห้องนอนเล็ก: ปิดอยู่";
-            }
-            if($requestL3 == '{"result":"true","value":"1"}'){
-                $Lamp3 = "ไฟห้องรับแขก: เปิดอยู่";
-            }else{
-                $Lamp3 = "ไฟห้องรับแขก: ปิดอยู่";
-            }
-            if($requestA1 == '{"result":"true","value":"1"}'){
-                $Air1 = "แอร์ห้องนอนใหญ่: เปิดอยู่";
-            }else{
-                $Air1 = "แอร์ห้องนอนใหญ่: ปิดอยู่";
-            }
-            if($requestA2 == '{"result":"true","value":"1"}'){
-                $Air2 = "แอร์ห้องนอนเล็ก: เปิดอยู่";
-            }else{
-                $Air2 = "แอร์ห้องนอนเล็ก: ปิดอยู่";
-            }
-            if($requestA3 == '{"result":"true","value":"1"}'){
-                $Air3 = "แอร์ห้องรับแขก: เปิดอยู่";
-            }else{
-                $Air3 = "แอร์ห้องรับแขก: ปิดอยู่";
-            }
-            
-            $status = $Lamp1;
-            $status += $Lamp2;
-            $status += $Lamp3;
-            $status += $Air1;
-            $status += $Air2;
-            $status += $Air3;
-            $txtSt = $status;
-			$messages = ['type' => 'text','text' => $txtSt];
-
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = ['replyToken' => $replyToken,'messages' => [$messages],];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-		}
-
-            $ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-	}
+      echo $result . "\r\n";
+    }
+  }
 }
-echo "Ok";
+echo "OK";
