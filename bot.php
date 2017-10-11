3eb1912d6e1b0890c7b3fbfb718f2209b4f84278
MoreThrust <peeraphat1997@gmail.com>
2017-10-11 13:36:40 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,11 +18,8 @@ if (!is_null($events['events'])) {
       $messages = [
           
         {
-    "type": "location",
-    "title": "my location",
-    "address": "〒150-0002 東京都渋谷区渋谷２丁目２１−１",
-    "latitude": 35.65910807942215,
-    "longitude": 139.70372892916203
+    "type": "text",
+    "text": "Hello, world"
 }
         
       ];
